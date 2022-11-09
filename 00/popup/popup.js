5cb5c04131415a0d71b7954250e2ab7ceb51455b
Cosmosinu <cosmosinu2016@gmail.com>
2022-11-09 21:06:14 +0900

    팝업 리팩토링 및 검은 배경 클릭시 이벤트 추가

--- 00/popup/popup.js
+++ 00/popup/popup.js
@@ -4,27 +4,28 @@ const goLogin = document.querySelector('#goLogin');
 const popup = document.querySelector('#popup');
 const close = document.querySelector('#close');
 const send = document.querySelector('#send');
+let userEmailAddr = document.querySelector('#email').value;
+let userPassword = document.querySelector('#password').value;
 
 goLogin.addEventListener('click', () => {
- document.querySelector('.container p').classList.add('hide');
- popup.classList.add('show');
- popup.firstElementChild.classList.add('active');
+ popupOpen();
 });
 
 close.addEventListener('click', () => {
- document.querySelector('.container p').classList.remove('hide');
- popup.classList.remove('show');
- popup.firstElementChild.classList.remove('active');
+ popupInit();
+});
+
+popup.addEventListener('click', (e) => {
+ if (e.target == popup) {
+  popupInit();
+ }
 });
 
 send.addEventListener('click', (e) => {
- let userEmailAddr = document.querySelector('#email').value;
- let userPassword = document.querySelector('#password').value;
  if (userEmailAddr == '') {
   e.preventDefault();
   alert('이메일을 입력하세요');
  } else if (userPassword == '') {
-  console.log(userEmailAddr);
   e.preventDefault();
   alert('패스워드를 입력하세요');
  } else if (userPassword.length < 6) {
@@ -35,3 +36,15 @@ send.addEventListener('click', (e) => {
   alert('대문자가 하나 포함되어야 합니다.');
  }
 });
+
+function popupInit() {
+ document.querySelector('.container p').classList.remove('hide');
+ popup.classList.remove('show');
+ popup.firstElementChild.classList.remove('active');
+}
+
+function popupOpen() {
+ document.querySelector('.container p').classList.add('hide');
+ popup.classList.add('show');
+ popup.firstElementChild.classList.add('active');
+}

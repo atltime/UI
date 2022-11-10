c59ebc3eb2b7ac93332fdef69a791ff3bbac653c
Cosmosinu <cosmosinu2016@gmail.com>
2022-11-10 09:11:39 +0900

    added function

--- 00/select/select.js
+++ 00/select/select.js
@@ -1,21 +1,45 @@
 // 'use strict';
-let $userChoice = document.querySelector('#clothes');
-let $shirtSize = document.querySelector('#shirtSize');
+let userChoice = document.querySelector('#clothes');
+let createSelect = document.querySelectorAll('.form-select');
 
-$userChoice.addEventListener('input', () => {
- if ($userChoice.value == 'shirts') {
-  $shirtSize.classList.remove('hide');
- } else {
-  $shirtSize.classList.add('hide');
+userChoice.addEventListener('input', () => {
+ if (userChoice.value == 'hats') {
+  selectInit(57, 58);
+ } else if (userChoice.value == 'shirts') {
+  selectInit(95, 100);
+ } else if (userChoice.value == 'pants') {
+  selectInit(29, 30);
  }
 });
+
+function selectInit(value1, value2) {
+ createSelect[1].innerHtml = '';
+ createSelect[1].classList.remove('hide');
+ let template = `
+    <option value="${value1}">${value1}</option>
+    <option value="${value2}">${value2}</option>
+`;
+ createSelect[1].innerHTML = template;
+}
 
 //jQuery라면
 // $('#clothes').on('input', function (e) {
 //  let value = $(this).val();
 //  if (value == 'shirts') {
-//   $('#shirtSize').removeClass('hide');
-//  } else {
-//   $('#shirtSize').addClass('hide');
+//   selectInit(95, 100);
+//  } else if (value == 'pants') {
+//   selectInit(28, 30);
+//  } else if (value == 'hats') {
+//   selectInit(57, 58);
 //  }
 // });
+
+// function selectInit(value1, value2) {
+//  $('.form-select').eq(1).removeClass('hide');
+//  $('.form-select').eq(1).html('');
+//  let template = `
+//       <option value="${value1}">${value1}</option>
+//       <option value="${value2}">${value2}</option>
+//     `;
+//  $('.form-select').eq(1).append(template);
+// }

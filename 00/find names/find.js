f3f2096dad065b3ec7bb419897eef7fdeb236bed
Cosmosinu <cosmosinu2016@gmail.com>
2022-11-12 16:40:33 +0900

    changed roles to actors

--- 00/find names/find.js
+++ 00/find names/find.js
@@ -11,19 +11,19 @@ check.addEventListener('click', function () {
 });
 
 function findNames(name) {
- for (let i = 0; i < roles.length; i++) {
+ for (let i = 0; i < actors.length; i++) {
   if (name.length <= 0) {
    result.innerHTML = `
-    <p style="text-align:center;font-size:14px;line-height:1.4">입력을 해주세요 >.<<br> "장태상 / 윤채옥 / 마에다 / 나월댁 / 윤종원 / 윤채옥"</p>
+    <p style="text-align:center;font-size:14px;line-height:1.4">입력을 해주세요 >.<<br> "박서준, 한소희, 수현, 김해숙, 조한철, 위하준"</p>
   `;
-  } else if (name == roles[i]) {
+  } else if (name == actors[i]) {
    result.innerHTML = `
-      입력하신 배역 "${name}"은 배우 "${actors[i]}"(으)로 등장인물이 맞습니다.
+      입력하신 "${name}"님 배우는 "${roles[i]}" 역으로 출연합니다.
     `;
    break;
   } else {
    result.innerHTML = `
-      입력하신 배역 "${name}"은 등장인물이 아닙니다.
+      입력하신 "${name}"님은 출연진이 아닙니다.
     `;
   }
  }

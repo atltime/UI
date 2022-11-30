85573f7b2ae11e549f6a75f76f119457e41c3194
Cosmosinu <cosmosinu2016@gmail.com>
2022-11-30 23:02:47 +0900

    Rotating navigation

---
+++ 02_50 projects/03_rotating nav animation/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+const container = document.querySelector('.container');
+
+open.addEventListener('click', () => container.classList.add('show-nav'));
+
+close.addEventListener('click', () => container.classList.remove('show-nav'));

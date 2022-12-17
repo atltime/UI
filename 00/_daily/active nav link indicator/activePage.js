15b0ccb6fbce4fb90922d58b05ebc85b4beaad62
Cosmosinu <cosmosinu2016@gmail.com>
2022-12-17 14:28:08 +0900

    pathname || href.includes

--- 00/_daily/active nav link indicator/activePage.js
+++ 00/_daily/active nav link indicator/activePage.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const activePage = window.location.pathname;
+const activePage = document.location;
 const navLinks = document.querySelectorAll('nav a');
+
 navLinks.forEach((link) => {
- if (link.href.includes(`${activePage}`)) {
+ console.log(activePage.pathname);
+ console.log(link.pathname);
+ if (link.pathname === activePage.pathname) {
   link.closest('li').classList.add('active');
  }
+
+ // 특정 문자열을 포함하는지 확인하는 메서드
+ //  if (link.href.includes(`${activePage}`)) {
+ //   link.closest('li').classList.add('active');
+ //  }
 });
